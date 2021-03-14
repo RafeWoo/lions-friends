e57e8885db7378a9dd6bc9529febedb2b932a19b
David Woo <david@familywoo.com>
2021-03-14 21:59:27 +0000

    Got user options saving out and displaying

--- lions-friends.php
+++ lions-friends.php
@@ -10,6 +10,9 @@ License: MIT
 Text Domain: lions-friends
 */
 
+// THis is a list of the option keys
+$lf_option_keys = array('update-all', 'update-lotto', 'update-tri', 'update-cinema', 'update-santa');
+
 
 //redirects user to a particular page after login
 //redirects admins to the dashboard and other users to the homepage. Make sure you use add_filter outside of is_admin(), since that function is not available when the filter is called.
@@ -39,13 +42,141 @@ add_shortcode( 'lf-user-login', 'lf_display_user_login');
 
 
 function lf_display_user_options() {
+	
+	
+	
 	if ( is_user_logged_in() ) { 
 	
-		//TODO display the user options
-		echo " <h1>Hello World</h1>";	
+		$wp_nonce = wp_create_nonce( 'wp_rest' );
+		$rest_url = get_site_url()."/wp-json/"."lions-friends/v1/update-options"; 
+	
+		$user = wp_get_current_user();
+		
+		echo "<p>hello $user->user_login</p>";
+		
+
+		?>
+		
+		<p>Please keep me informed about:</p>
+		<form name="lf-options-form" class="lf-options-form" onsubmit="return updateOptions();">
+				
+			<label for="update-all">Everything: </label><input type="checkbox" id="update-all" name="update-all" /><br>
+			<label for="update-lottery">Southam Lions 500 Club </label><input type="checkbox" id="update-lotto" name="update-lotto" /><br>
+			<label for="update-triathlon">Southam Triathlon </label><input type="checkbox" id="update-tri" name="update-tri" /><br>			
+			<label for="update-cinema">Southam Cinema </label><input type="checkbox" id="update-cinema" name="update-cinema" /><br>
+			<label for="update-santa">Santa </label><input type="checkbox" id="update-santa" name="update-santa" /><br>
+	
+		<br><br>		
+			<input id="lf-submit-options" type="submit" value="Update" disabled="true" /> 
+		
+		</form>
+		<p id="options_error"></p>
+		
+		<script type="application/javascript">
+		<?php
+		
+			$update_flags = array();
+			
+			global $lf_option_keys;
+			foreach($lf_option_keys as $key) {
+				$update_flags[$key] = (bool) get_user_meta( $user->ID, $key, true );				
+			}
+		?>
+		    const user_options = <?php echo json_encode($update_flags); ?>;
+			
+			
+			//populate
+			for( const [key, value] of Object.entries(user_options) ) {							
+				document.getElementById(key).checked = value;
+			}
+			
+			
+		
+			//disable options if all is selected?
+			document.forms["lf-options-form"].addEventListener('change', function() {
+				
+				let is_modified = false;
+				for( const [key, value] of Object.entries(user_options) ) {							
+					if( value != document.getElementById(key).checked )
+					{
+						is_modified = true;
+						break;
+					}
+				}
+				
+				//alert("is modified" + is_modified);
+				
+				document.getElementById("lf-submit-options").disabled = !is_modified;
+			});	
+
+		function updateOptions() {
+			
+			const url = <?php echo "\"$rest_url\"" ?>;					
+			const nonce = <?php	echo "\"$wp_nonce\"" ?>;
+			
+			let formdata = document.forms["lf-options-form"];
+			let result_element = document.getElementById("options_error");
+			
+			fetch(	
+				url, 
+				{
+					method: 'POST',
+					body: new URLSearchParams(new FormData(formdata)),
+					headers: {
+						'X-WP-Nonce': nonce			
+					},
+				}
+			).then(
+				(resp) => {
+					return resp.json(); // or resp.text() or whatever the server sends
+				}
+			).then(
+				(body) => {
+					
+					
+					
+					if( body.success )
+					{
+						//result_element.innerHTML = "success " 
+						//location.reload();
+						location.reload();
+					}
+					else{
+						
+						//assume WP_ERROR
+						//let keys = "keys: ";
+						//for( const [key,value] of Object.entries(body) )
+						//{
+						//	keys += key + " : " + value + " , ";
+						//}
+						//alert("error fdh");
+						result_element.innerHTML = body.message;//.hello;
+					}
+					
+					
+				}
+			).catch(
+				(error) => {	
+					
+					
+						//alert("error fdh");
+						result_element.innerHTML = error.message;//.hello;
+				
+					//alert("error2");	
+					//result_element.innerHTML = error. "Error updating";					
+				}
+								
+			);
+			
+			
+			return false;
+		}
+		</script>
+		
+		<?php
 		wp_loginout( home_url() ); // Display "Log Out" link.
-		echo " | ";
-		wp_register('', ''); // Display "Site Admin" link.
+		//echo " | ";
+		//wp_register('', ''); // Display "Site Admin" link.
 	}
 }
 
@@ -59,7 +190,7 @@ function lf_display_user_sign_up()
 	
 	?>
 	<h2>Sign Up</h2>
-	<script>
+	<script type="application/javascript">
 		function validateForm() {
 			
 		
@@ -67,9 +198,9 @@ function lf_display_user_sign_up()
 			document.getElementById("email-error").innerHTML ="";
 			document.getElementById("password-error").innerHTML ="";			
 		
-			var formdata = document.forms["signup-form"];
+			let formdata = document.forms["signup-form"];
 			
-			var form_is_valid = true;
+			let form_is_valid = true;
 			
 			if (formdata["username"].value == "") {
 						
@@ -109,20 +240,23 @@ function lf_display_user_sign_up()
 
 			if( form_is_valid )
 			{
-				var result_element = document.getElementById("result");
+				let result_element = document.getElementById("result");
 				result_element.innerHTML = "form valid";
 			
-			<?php
-				echo "const url = \"$rest_url\"";
-			?>
 			
+			const url = <?php echo "\"$rest_url\"" ?>;					
+			const nonce = <?php	echo "\"$wp_nonce\"" ?>;							
 			
+					
 			result_element.innerHTML = "sent data";
 			fetch(	
 				url, 
 				{
 					method: 'POST',
-					body: new URLSearchParams(new FormData(formdata)) 
+					body: new URLSearchParams(new FormData(formdata)),
+					headers: {
+						'X-WP-Nonce': nonce			
+					},
 				}
 			).then(
 				(resp) => {
@@ -137,9 +271,9 @@ function lf_display_user_sign_up()
 						location.reload();
 					}
 					else{
-						var error_string = "Failed to register: ";
-						var fail_reasons = "";
-						var count = 0;
+						let error_string = "Failed to register: ";
+						let fail_reasons = "";
+						let count = 0;
 						for( const reason of body.fail_reasons )
 						{
 							if( count > 0)
@@ -183,16 +317,14 @@ function lf_display_user_sign_up()
 		<input type="text" id="username" name="username" value=""/><p id="username-error"></p>
 		
 		<label for="email">e-mail:</label><br>
-		<input type="text" id="email" name="email" value="" /><p id="email-error"></p>
+		<input type="email" id="email" name="email" value="" /><p id="email-error"></p>
 		
 		<label for="password">password</label><br>
 		<input type="password" id="password" name="password" value=""/><p id="password-error"></p>
 		
 		<label for="confirm_pw">confirm password:</label><br>
 		<input type="password" id="confirm_pw" name="confirm_pw" value="" />
-	<?php
-		echo "<input type=\"hidden\" id=\"nonce\" name=\"nonce\" value=\"$wp_nonce\" />";		
-	?>
+	
 		<br><br>		
 		<input type="submit" value="Submit">
 	</form> 
@@ -261,19 +393,63 @@ function lf_display_user_login()
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
+//permission callbacks
+function lf_is_member() {
+    // Restrict endpoint to only users who have the edit_posts capability.
+	
+    if ( !current_user_can( 'read' ) ) 
+	{
+      return new WP_Error( 'rest_forbidden', esc_html__( 'Error - insufficient permissions.', 'lions-friends' ), array( 'status' => 401 ) );
+	}
+ 
+    // This is a black-listing approach. You could alternatively do this via white-listing, by returning false here and changing the permissions check.
+    return true;
+}
+
 
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'lions-friends/v1', '/create-user', 
 		array(
 			'methods' => 'POST',
-			'callback' => 'try_create_friend',
+			'callback' => 'lf_try_create_friend',
 			'permission_callback' => '__return_true',		
 		)	
 	);
+	
+	register_rest_route( 'lions-friends/v1', '/update-options', 
+		array(
+			'methods' => 'POST',
+			'callback' => 'lf_update_options',
+			'permission_callback' => 'lf_is_member',				
+		)
+	);	
+	
 	}
 );
 
-function try_create_friend( WP_REST_Request $request )
+function lf_update_options( WP_REST_Request $request )
+{
+	global $lf_option_keys;
+	
+	$user_id = get_current_user_id();	
+	$new_options = [];
+	foreach($lf_option_keys as $key) {
+		$new_options[$key] = (bool) $request->get_param( $key );
+	}
+	
+	foreach ($new_options as $meta_key => $meta_value) {	
+		update_user_meta( $user_id, $meta_key, $meta_value );
+	}
+	
+	
+	$success = true;
+	
+	return array( 
+	    'success' => $success,		
+	);
+}
+
+function lf_try_create_friend( WP_REST_Request $request )
 {
 		
 	$user_name = sanitize_user( $request->get_param( 'username'), true );
